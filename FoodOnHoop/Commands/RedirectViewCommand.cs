30e44120456bd25673a29c0dc61e128248d1f9cc
New User <New User@LAPTOP-IJI0NIKR>
2022-03-02 21:45:10 +0530

    Add project files.

---
+++ FoodOnHoop/Commands/RedirectViewCommand.cs
@@ -0,0 +1,61 @@
+﻿using FoodOnHoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FoodOnHoop.Commands
+{
+    public class RedirectViewCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public MainViewModel mainview;
+        public RedirectViewCommand(MainViewModel viewModel)
+        {
+            this.mainview = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Home")
+            {
+                mainview.SelectViewModel = new HomeViewModel();
+            }
+            else if (parameter.ToString() == "AllTimeChillers")
+            {
+                mainview.SelectViewModel = new AllTimeChillersViewModel();
+            }
+            else if (parameter.ToString() == "ContactUs")
+            {
+                mainview.SelectViewModel = new ContactUsViewModel();
+            }
+            else if (parameter.ToString() == "AboutUs")
+            {
+                mainview.SelectViewModel = new AboutUsViewModel();
+            }
+            else if (parameter.ToString() == "FoodOnMood")
+            {
+                mainview.SelectViewModel = new FoodOnMoodViewModel();
+            }
+            else if (parameter.ToString() == "HotClassic")
+            {
+                mainview.SelectViewModel = new HotClassicViewModel();
+            }
+            else if (parameter.ToString() == "Login")
+            {
+                mainview.SelectViewModel = new LoginViewModel();
+            }
+            //else if (parameter.ToString() == "SignUp")
+            //{
+            //    mainview.SelectViewModel = new LoginViewModel();
+            //}
+        }
+    }
+}
---
+++ FoodOnHoop/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FoodOnHoop.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FoodOnHoop.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectViewModel = new HomeViewModel();
+        public MainViewModel()
+        {
+            RedirectViewCommands = new RedirectViewCommand(this);
+        }
+        public BaseViewModel SelectViewModel
+        {
+            get { return _selectViewModel; }
+            set { _selectViewModel = value; OnPropertyChanged(nameof(SelectViewModel)); }
+        }
+        public ICommand RedirectViewCommands { get; set; }
+    }
+}
